ef4d72e4303901b95dc096249391a37983fc1185
Your Name <charlenetanataaa@gmail.com>
2026-02-05 10:22:50 +0700

    Create User Entity

---
+++ src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    // @OneToMany(() => Post, post => post.user)
+    // posts: Post[];
+}
